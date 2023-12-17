23626adb549b017fdbe2bcc5244abc2bebf95c35
Canyon Zhang <canyonzh@usc.edu>
2023-12-17 00:22:15 -0500

    added contact button:

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,10 +6,11 @@ import { motion } from "framer-motion";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Intro() {
     return (
-        <section className="mb-28 max-w-[50rem] mx-auto text-center sm:mb-0 scroll-mt-[100rem]">
+        <section  className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
         <div className="flex items-center justify-center">
         <div className="relative">
         <motion.div
@@ -58,6 +59,25 @@ export default function Intro() {
         <span className="font-bold">entering my last semester at USC.</span> I am always looking to meet and learn
         from others and I would love to connect.
       </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+            Contact me here{" "}
+            <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+
+          </Link>
+
+      </motion.div>
+      
 
       </section>
     );
