b67cdcef70e11641951584cb1a18c0566fadff92
Canyon Zhang <canyonzh@usc.edu>
2023-12-17 00:03:12 -0500

    added motions for intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,19 +1,48 @@
+"use client";
+
 import React from 'react'
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Intro() {
     return (
       <section>
         <div className="flex items-center justify-center">
-          <div>
+        <div className="relative">
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
             <Image
               src="/Profile-min.png"
-              fill={true}
               alt="Canyon portrait"
               quality="95"
               priority={true}
+              width="192"
+              height="192"
+              className="h-42 w-40 rounded-full border-[0.35rem] border-white
+              shadow-xl"
             />
-          </div>
+          </motion.div>
+          
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
         </div>
       </section>
     );
