fdad18ba0d11ab864a02ab4758b588d3c9b9d3f5
Canyon Zhang <canyonzh@usc.edu>
2023-12-17 17:51:12 -0500

    first 3 sections nav bar responsive

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import Project from "./project";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Projects() {
+  const { ref, inView } = useInView({threshold: 0.5}); // only consider the section in view if it is 75 percent in view
+  const {setActiveSection} = useActiveSectionContext();
+
+    useEffect(() => {
+      if (inView) {
+        setActiveSection("Projects");
+      }
+      console.log(inView);
+    }, [inView, setActiveSection]); 
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
