7a2bbf6be56e9afbaa42988ab2ad6aff869a6522
Canyon Zhang <canyonzh@usc.edu>
2023-12-17 01:27:21 -0500

    started adding project images

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -17,6 +18,19 @@ export default function Projects() {
   );
 }
 
-function Project({title, description, tags, imageUrl}) {
-    return <div>{title}</div>
+type ProjectProps = typeof projectsData[number];
+
+function Project({title, description, tags, imageUrl}: ProjectProps) {
+    return <section className="bg-gray">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <ul>
+            {tags.map((tag, index) => (
+                <li key={index}>{tag}</li>
+            ))}
+        </ul>
+        <Image width={400} height={400} src={imageUrl} alt={title} quality={95}/>
+
+    </section>
+
 }
