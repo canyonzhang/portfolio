7b600d84fd6b17293cd256b81c0cd0108b09426b
Canyon Zhang <canyonzh@usc.edu>
2023-12-18 16:30:07 -0500

    fixed light mode

--- components/intro.tsx
+++ components/intro.tsx
@@ -76,8 +76,8 @@ export default function Intro() {
       >
         <Link
         href="#contact"
-        
-        className="group bg-gray-100 text-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-50 active:scale-105 transition "
+      
+        className=" group bg-black text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition borderBlack dark:bg-gray/10"
         onClick = {() => {setActiveSection("Contact")
           setTimeOfLastClick(Date.now())
         }}>
@@ -86,7 +86,7 @@ export default function Intro() {
         </Link>
             
             <a
-                className="group bg-black text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition borderBlack dark:bg-gray/10"
+                className="group bg-gray-200 text-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-300 active:scale-105 transition"
                 href="/resume.pdf"
                 download
                 >
@@ -95,7 +95,7 @@ export default function Intro() {
                 </a>
 
                 <a
-                className="bg-white p-4 text-gray-100 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-100 active:scale-105 transition cursor-pointer borderWhite dark:bg-white/10 dark:text-white/60"
+                className="bg-gray-200 p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
                 href="https://www.linkedin.com/in/canyon-zhang-86231a208/"
                 target="_blank"
                 >
@@ -103,7 +103,7 @@ export default function Intro() {
                 </a>
 
                 <a
-                className="bg-white p-4 text-gray-100 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-100 active:scale-105 transition cursor-pointer borderWhite dark:bg-white/10 dark:text-white/60"
+                className="bg-gray-200 p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
                 href="https://github.com/canyonzhang"
                 target="_blank"
                 >
