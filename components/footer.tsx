a6a00fa3fc0a25fb1ad27aa4720cf6c77499c1cd
Canyon Zhang <canyonzh@usc.edu>
2023-12-17 23:41:22 -0500

    finished header

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2024 Canyon Zhang. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">Portfolio Site:</span> Built with React, Next.js, and TypeScript, styled with Tailwind CSS, animated with Framer Motion, and equipped with email integration through React Email and Resend; 
+        deployed and hosted on Vercel.
+      </p>
+    </footer>
+  );
+}
