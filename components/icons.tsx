67a481a5404004e29e14e2fa1633ba9404470e34
Canyon Zhang <canyonzh@usc.edu>
2023-12-18 23:43:13 -0500

    adjusting icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,15 +1,15 @@
 export const AstraZenecaIcon = () => {
   return (
-    <div className="flex items-center justify-center mt-3">
-      <img src="/astrazeneca.svg" alt="AstraZeneca" className="h-8 w-8" />
+    <div className="flex items-center justify-center mt-3 ml-3.5 sm:mt-3 sm:h-8 sm:w-8 h-8 w-8">
+      <img src="/astrazeneca.svg" alt="AstraZeneca" className="object-contain max-h-full max-w-full" />
     </div>
   );
 };
 
 export const USCIcon = () => {
   return (
-    <div className="flex items-center justify-center mt-3.5 ml-0.2">
-      <img src="/usc.png" alt="USC" className="h-8 w-5" />
+    <div className="flex items-center justify-center mt-3.5 ml-3.5 sm:mt-3.5 sm:h-8 sm:w-8 h-8 w-8">
+      <img src="/usc.png" alt="USC" className="object-contain max-h-full max-w-full" />
     </div>
   );
 };
