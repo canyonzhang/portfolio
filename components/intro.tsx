19a0f4a04b9ccf789483bb069f01455431deb40e
Canyon Zhang <canyonzh@usc.edu>
2024-02-20 10:44:07 -0800

    changed intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,7 +63,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hey, I'm Canyon.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span>{" "}
-        <span className="font-bold">entering my last semester at USC.</span> I'm passionate about connecting and exchanging ideas with others, 
+        <span className="font-bold">in my last semester at USC.</span> I'm always looking to connect and exchange ideas with others, 
         so feel free to get in touch!
       </motion.h1>
 
